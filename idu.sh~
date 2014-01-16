88034113e174414ff0298a9ed3071ee7cc640d03
omtinez <omtinez@gmail.com>
2014-01-16 02:42:15 +0000

    clean up and new folder structure

--- idu.sh~
+++
@@ -1,2 +0,0 @@
-cp Makefile.idu Makefile
-make clean && make all && sudo make flash PORT=/dev/ttyUSB0
--- nano.sh
+++
@@ -1,2 +0,0 @@
-cp Makefile.nano Makefile
-make clean && make all && sudo make flash PORT=/dev/ttyUSB1
